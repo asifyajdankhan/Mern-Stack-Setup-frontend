2e4206180ec013e4abf073ae3ed8a751c45fc915
MdAsifKhan <asifyajdankhan416@gmail.com>
2023-07-17 16:44:10 +0530

    completed the register form and some changes in backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://127.0.0.1:27017/ECOM-MVC", {
@@ -14,10 +16,17 @@ mongoose
     console.log(error);
   });
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  })
+);
+
 const user_routes = require("./routes/route");
 
 app.use("/api", user_routes);
 
-app.listen(3000, function () {
-  console.log("Server is running on the port no:3000");
+app.listen(5000, function () {
+  console.log("Server is running on the port no:5000");
 });
